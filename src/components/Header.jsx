710476a1827ea1c4bf0e38f538d6968e0f1274f7
Kaela Nguyen <kaedtn96@gmail.com>
2022-12-06 21:23:28 -0800

    install tailwind and create header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react'; 
+import { Icon } from '@iconify/react'; 
+import broadcast from '@iconify/icons-mdi/broadcast'; 
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="bg-[#1425ba] text-[#fff] text-4xl py-4 font-bold">
+      <h1 className="text-center"><Icon className="inline-block text-[#cf1711]" icon={broadcast}></Icon> Natural Events Tracker<Icon className="inline-block text-[#cf1711]" icon={broadcast}></Icon></h1>
+      <h4 className="text-lg text-center">(Powered by NASA) </h4>
+    </div>
   )
 }
 
